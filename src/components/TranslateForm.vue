29e573991c1d34a4995486cbc4443a8a4e9b5493
Jordan Blakey <jordan@jordanblakey.com>
2017-07-30 23:24:42 -0500

    Translation working in 4 languages and styled with boostrap.

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row" id="TranslateForm">
+    <div class="col-md-5 col-md-offset-3">
+
+    <form id="transForm" class="form-inline well" v-on:submit="formSubmit">
+      <input class="form-control" type="text" v-model="textToTranslate" placeholder="Enter a word...">
+      <select class="form-control" v-model="language">
+        <option value="ru">Russian</option>
+        <option value="es">Spanish</option>
+        <option value="fr">French</option>
+        <option value="zh">Chinese</option>
+      </select>
+      <input class="btn btn-primary" type="submit" value="translate">
+    </form>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data(){
+    return {
+      textToTranslate:'',
+      language:''
+    }
+  },
+  created() {
+    this.language = 'ru';
+  },
+  methods: {
+    formSubmit(e){
+      this.$emit('formSubmit', this.textToTranslate, this.language);
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+  #transForm > * {
+    margin: 0 5px;
+  }
+</style>
---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2 class="text-primary">{{translatedText}}</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'translateOutput',
+  props:['translatedText'],
+  methods: {
+    formSubmit(e){
+      this.$emit('formSubmit', this.textToTranslate);
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
